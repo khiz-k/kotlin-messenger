eec5742f9fbe200c78e3275d733745dc8bffe0a8
Khizar Khan <khiz_k@outlook.com>
2020-09-13 18:39:15 -0700

    init

---
+++ app/src/main/java/com/khiz/kotlinmessenger/models/User.kt
@@ -0,0 +1,9 @@
+package com.khiz.kotlinmessenger.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class User(val uid: String, val username: String, val profileImageUrl: String): Parcelable {
+  constructor() : this("", "", "")
+}
